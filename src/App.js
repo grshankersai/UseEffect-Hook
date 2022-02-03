61d12129fec0b9dac117fd466edbf072f36ab552
Shanker <grshankersai@gmail.com>
2022-02-03 12:24:06 +0530

    03-Feb-2022 12:23 AM :: Final Commit for UseEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+
+
+      
+    return ()=>{
+     console.log("clean-Up"); 
+    }
+  }, [resourceType]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{windowWidth}</div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <hr />
+      <hr />
+      <button
+        className={"button"}
+        onClick={() => {
+          setResourceType("Posts");
+        }}
+      >
+        Posts
+      </button>
+      <button
+        className={"button"}
+        onClick={() => {
+          setResourceType("Users");
+        }}
+      >
+        Users
+      </button>
+      <button
+        className={"button"}
+        onClick={() => {
+          setResourceType("Comments");
+        }}
+      >
+        Comments
+      </button>
+      <h2>{resourceType}</h2>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
     </div>
   );
 }
